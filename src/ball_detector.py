41e323e41bab653550767ddf6fef599c665b44d1
qhdwight <qhdwight@gmail.com>
2024-03-30 17:27:36 -0400

    Comments

--- src/ball_detector.py
+++ src/ball_detector.py
@@ -3,18 +3,27 @@
 import rospy
 import cv2 as cv
 
-from geometry_msgs.msg import Pose2D
+from tf2_ros import TransformBroadcaster
 from sensor_msgs.msg import Image
+
+broadcaster = TransformBroadcaster()
 
 
 def image_callback(data: Image) -> None:
+    # TODO:
+    # 1. convert to opencv using cv_bridge
+    # 2. convert to hsv
+    # 3. apply thresholding
+    # 4. find contours
+    # 5. find circle
+    # 6. convert to map coordinates
+    # 7. publish the circle as a transform "ball" in "map" using "broadcaster"
     pass
 
 
 def ball_detector() -> None:
     rospy.init_node('ball_detector')
     rospy.Subscriber('/image', Image, image_callback)
-    rospy.Publisher('/ball_pose', Pose2D)
     rospy.spin()
 
 

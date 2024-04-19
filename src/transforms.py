9a22e9b8b604511e3dbf841daa965884b89c09ba
qhdwight <qhdwight@gmail.com>
2024-04-19 16:55:29 -0400

    More comments

--- src/transforms.py
+++ src/transforms.py
@@ -7,8 +7,12 @@ import math
 def to_se2(t: TransformStamped) -> SE2:
     # Quaternions are fundamentally angle-axis representations
     # We have some what of a restricted case since we're only rotating about the z-axis, therefore:
+    #
+    # x ~= 0
+    # y ~= 0
     # z = sin(theta / 2)
     # w = cos(theta / 2)
+    #
     # It follows that theta = 2 * arccos(w)
     # But the range of arccos is [0, pi], so we need to add the sign of z to get the full range of rotation
     angle = math.copysign(2 * math.acos(t.transform.rotation.w), t.transform.rotation.z)

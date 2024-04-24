abd59e14a3b6d554124c5c82fe4ccfc99ad570fb
qhdwight <qhdwight@gmail.com>
2024-04-23 21:23:16 -0400

    Update

--- src/motion_planner.py
+++ src/motion_planner.py
@@ -3,8 +3,10 @@
 from manifpy import SE2, SE2Tangent
 
 from geometry_msgs.msg import Twist
-from tf2_ros import Buffer, TransformListener
+from tf2_ros import Buffer, TransformListener, TransformBroadcaster
 from transforms import *
+
+import numpy as np
 
 
 def motion_planner() -> None:
@@ -13,25 +15,30 @@ def motion_planner() -> None:
     tf2_buffer = Buffer()
     TransformListener(tf2_buffer)
 
+    broadcaster = TransformBroadcaster()
+
     number = rospy.get_param('~number', 0)
     cmd_vel_topic = f'cmd_vel_{number}'
     cmd_vel_pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size=1)
 
-    K = 512  # Number of trajectories to sample
+    K = 2048  # Number of trajectories to sample
     T = 32  # Size of trajectory horizon
 
-    sigma = np.diag([0.2, 1])
+    sigma = np.diag([0.3, 0.4])
+    sigma_inv = np.linalg.inv(sigma)
 
     alpha = 1
-    lambda_ = 50
+    lambda_ = 20
     gamma = lambda_ * (1 - alpha)
 
     commands = np.zeros((T, 2))
 
     def terminal_cost(bot_in_map: SE2, goal_in_map: SE2) -> float:
         d = goal_in_map - bot_in_map
-        cost = np.linalg.norm(d.coeffs())
-        return cost
+        coeffs = d.coeffs()
+        coeffs *= [2, 2, 1]
+        coeffs = np.clip(coeffs, [-1, -1, -np.pi], [1, 1, np.pi])
+        return np.linalg.norm(coeffs, ord=2)
 
     def mppi(bot_in_map: SE2, goal_in_map: SE2) -> Twist:
         nonlocal commands
@@ -51,32 +58,45 @@ def motion_planner() -> None:
 
                 sample_command = SE2Tangent(sample_commands[k, t, 0], 0, sample_commands[k, t, 1])
 
-                sample_final_pose = sample_final_pose + sample_command
+                sample_final_pose = sample_final_pose + (sample_command / 10)
 
-                costs[k] += gamma * (commands[t].T @ np.linalg.inv(sigma) @ sample_commands[k, t])
+                # costs[k] += gamma * (commands[t].T @ sigma_inv @ sample_commands[k, t])
 
-            costs[k] = terminal_cost(sample_final_pose, goal_in_map)
+            costs[k] += terminal_cost(sample_final_pose, goal_in_map)
 
         rho = costs.min()
 
-        temp = np.exp((-1 / lambda_) * (costs - rho))
+        soft_maxed = np.exp((-1 / lambda_) * (costs - rho))
 
-        eta = np.sum(temp)
-
-        w = (1 / eta) * temp
+        w = soft_maxed / np.sum(soft_maxed)
 
         w_epsilon = np.average(sample_perturbations, axis=0, weights=w)
 
         commands += w_epsilon
 
+        x = bot_in_map
+        for t in range(T):
+            x = x + SE2Tangent(commands[t, 0], 0, commands[t, 1]) / 10
+            if t % 4 == 0:
+                broadcaster.sendTransform(to_tf(x, "map", f"bot_{number}_mppi_{t}"))
+
+        v, w = commands[0]
+
+        print(v, w)
+
         commands = np.roll(commands, -1, axis=0)
+        commands[-1] = np.zeros(2)
 
-        print(commands[0])
+        # return Twist(
+        #     linear=Vector3(x=sample_commands[np.argmin(costs), 0, 0]),
+        #     angular=Vector3(z=sample_commands[np.argmin(costs), 0, 1])
+        # )
 
-        return Twist(
-            linear=Vector3(x=commands[0, 0]),
-            angular=Vector3(z=commands[0, 1])
-        )
+        return Twist()
+        # return Twist(
+        #     linear=Vector3(x=v),
+        #     angular=Vector3(z=w)
+        # )
 
     # def control_law(bot_in_map: SE2, goal_in_map: SE2) -> Twist:
     #     tangent = goal_in_map - bot_in_map
@@ -91,7 +111,7 @@ def motion_planner() -> None:
     #         angular=Vector3(z=w)
     #     )
 
-    rate = rospy.Rate(30)
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         try:
             goal_in_map = to_se2(tf2_buffer.lookup_transform("map", f"goal_{number}", rospy.Time(0)))

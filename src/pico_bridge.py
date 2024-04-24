070728451949a69749e7effff8a2760cad2cde3b
Rajin Nagpal <rnagpal02@outlook.com>
2024-04-24 01:01:50 +0000

    Implement pico drive bridge for ros

--- src/pico_bridge.py
+++ src/pico_bridge.py
@@ -6,25 +6,20 @@ import serial
 import struct
 
 def diff_drive() -> None:
-    # rospy.init_node("diff_drive")
-
-    # number = rospy.get_param("~number", 0)
-
-    # def twist_callback(message: Twist) -> None:
-    #     ...
-    #     # TODO: Talk to pico with pyserial
-
-    # cmd_vel_topic = f"cmd_vel_{number}"
-    # rospy.Subscriber(cmd_vel_topic, Twist, twist_callback)
-
-    # rospy.spin()
-
-    print("hi")
+    rospy.init_node("diff_drive")
+    number = rospy.get_param("~number", 0)
     ser = serial.Serial('/dev/ttyACM1', 115200)
     print(ser.name)
-    data = struct.pack('<BBffB', 0xAB, 0xCD, 0.0, 0.0, 0xEF)
-    print(data)
-    print(ser.write(data))
+
+    def twist_callback(message: Twist) -> None:
+        lin_vel = message.linear.x
+        ang_vel = message.angular.z
+        ser.write(struct.pack('<BBffB', 0xAB, 0xCD, lin_vel, ang_vel, 0xEF))
+
+    cmd_vel_topic = f"cmd_vel_{number}"
+    rospy.Subscriber(cmd_vel_topic, Twist, twist_callback)
+
+    rospy.spin()
 
 if __name__ == "__main__":
     try:

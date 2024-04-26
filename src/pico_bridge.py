64e08ec90dd9af94d1003ccaaf4491238ad940f2
Rajin Nagpal <rnagpal02@outlook.com>
2024-04-26 05:44:25 +0000

    Update udev

--- src/pico_bridge.py
+++ src/pico_bridge.py
@@ -14,8 +14,8 @@ MOTOR_CMD_TIMEOUT = 1
 def pico_bridge() -> None:
     rospy.init_node("pico_bridge")
     number = rospy.get_param("~number", 0)
-    drive_ser = serial.Serial('/dev/pico_drive', 115200)
-    ball_ser = serial.Serial('/dev/pico_ball', 115200)
+    drive_ser = serial.Serial('/dev/pico_drive_2', 115200)
+    ball_ser = serial.Serial('/dev/pico_ball_2', 115200)
     print(drive_ser)
     print(ball_ser)
 
